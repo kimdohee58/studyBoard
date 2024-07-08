22bb061c818efdfc3b6df8889c5a510c9d69b5d7
kimdohee58 <kimdohee58@gmail.com>
2024-07-08 12:08:21 +0900

    240708

---
+++ studyBoard/src/main/java/com/nc13/study/board/StudyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.nc13.study.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBoardApplication.class, args);
+	}
+
+}
